530307d43e37dcd7c0325ed3b39fb96cb6bd7109
Journey <timtag1190@gmail.com>
2022-06-24 01:10:25 -0500

    Add `ASF Redeem Helper` and `ASF AddLicense Helper`

---
+++ userscripts/meta/asf-redeemkey-helper.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          ASF Redeem Helper
+// @namespace     https://github.com/StylusThemes/Userscripts
+// @description   Make redeem key commands clickable.
+// @match         *://*.steamgifts.com/giveaways/won
+// @version       1.0
+// @grant         GM_xmlhttpRequest
+// @grant         GM_getValue
+// @grant         GM_setValue
+// ==/UserScript==
